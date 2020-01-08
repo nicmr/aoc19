fed60c05b2ffce9880d8b3490516462e05b86ab8
Nicolas Mohr <Nico.Mohr@gmx.net>
2020-01-08 16:02:03 +0100

    inital commit for Day2

---
+++ src/Day2.hs
@@ -0,0 +1,24 @@
+module Day2
+    ( task1
+    )
+    where
+
+
+
+
+import Text.Megaparsec
+import Text.Megaparsec.Char
+
+
+
+type Instruction = Int
+
+task1 :: String
+task1 = "done"
+
+
+-- instruction :: Parser Instruction
+-- instruction = do
+--     sepBy
+
+    
