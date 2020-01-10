5f19e6b64307463434ae4bf7c66f39234e8a63a6
Nicolas Mohr <Nico.Mohr@gmx.net>
2020-01-10 10:37:02 +0100

    basics for day3

---
+++ src/Day3.hs
@@ -0,0 +1,13 @@
+module Day3 where
+
+import qualified Data.ByteString as B
+import Data.Function ((&))
+
+
+task1 :: B.ByteString -> String
+task1 _ = 
+    "not yet implemented"
+
+task2 :: B.ByteString -> String
+task2 _ = 
+    "not yet implemented"
